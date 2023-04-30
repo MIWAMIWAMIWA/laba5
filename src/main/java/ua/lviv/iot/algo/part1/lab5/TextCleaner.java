52f287b16698a74fce2e6ec55f2533787bda7e35
MIWAMIWAMIWA <107461633+MIWAMIWAMIWA@users.noreply.github.com>
2023-04-30 12:27:47 +0300

    adding scanner

--- src/main/java/ua/lviv/iot/algo/part1/lab5/TextCleaner.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/TextCleaner.java
@@ -1,4 +1,5 @@
 package ua.lviv.iot.algo.part1.lab5;
+import java.util.Scanner;
 public class TextCleaner {
     public static final String HARMFULLLINKS =
             "(?i)(http://www.)([a-zA-Z0-9]*)(xxx|charmes|hacking|crack)([a-zA-Z0-9]*)(.com)";
@@ -8,13 +9,10 @@ public class TextCleaner {
         return textToClean.replaceAll(HARMFULLLINKS, FORDELETEDLINK);
     }
     public static void main(String[] args) {
-        String miwa = "gr3pnklgrnhttp://www.feg4xxxgr4fJ.com-" +
-                "grsfgfsggnerjngfknggmrwwwgrggr" +
-                "http://www.g35GcharmesfgrF.comwgkl_rfr" +
-                "grngerk -http://www.4ghackINGt4fb.com-fewfgwgwr " +
-                "fwkrnlgejeg-http://www.gddgcRACKgrgrsg.com";
+        Scanner myObj = new Scanner(System.in);
+        String input = myObj.nextLine();
         TextCleaner textCleaner = new TextCleaner();
-        miwa = textCleaner.textUrlCleaning(miwa);
-        System.out.println(miwa);
+        input = textCleaner.textUrlCleaning(input);
+        System.out.println(input);
     }
 }
